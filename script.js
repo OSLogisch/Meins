9fc3b1869f8df4a0c6e4c895fb09de1b4ea235c2
OSLogisch <96527187+OSLogisch@users.noreply.github.com>
2024-03-03 18:37:29 +0100

    00

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// URL der Seite von der wir den Body laden wollen
+let datum = new Date()
 const url = 'https://lgh.today/plan/2024-03-01:2'; 
 
 // Fetch Request
 fetch(url)
   .then(response => response.text())
   .then(html => {
-    // Den geholten HTML Code in unseren Body einfügen 
     document.body.innerHTML = html;
   });
