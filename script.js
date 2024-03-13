d49d7e9c783c0fafc8d461a8cd811b73ea86ea95
OSLogisch <96527187+OSLogisch@users.noreply.github.com>
2024-03-13 11:37:44 +0100

    2

--- script.js
+++ script.js
@@ -2,10 +2,3 @@ function submit() {
     let input = document.getElementById('input').value
     document.getElementById('output').textContent = input
 }
-
-function submit_enter(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        document.getElementById('input').submit();
-    }
-}
