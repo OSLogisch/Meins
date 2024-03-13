a83a340900f9ca9e76eab7e2363ff030919dfcc0
OSLogisch <96527187+OSLogisch@users.noreply.github.com>
2024-03-13 13:38:16 +0100

    1

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-function getUserName() {
-    var nameField = document.getElementById('nameField').value;
-    var result = document.getElementById('result');
-    
-    if (nameField.length < 3) {
-        result.textContent = 'Username must contain at least 3 characters';
-        //alert('Username must contain at least 3 characters');
-    } else {
-        result.textContent = 'Your username is: ' + nameField;
-        //alert(nameField);
-    }
-    }
+function submit() {
+    let input = document.getElementById('input')
+    document.getElementById('output').innerText = input.value
+}
