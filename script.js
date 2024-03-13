295d595c40f64bff6f8313fde79618b7e0f08af4
OSLogisch <96527187+OSLogisch@users.noreply.github.com>
2024-03-13 13:52:12 +0100

    1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function submit() {
     let input = document.getElementById('input').value
-    let output = document.getElementById('output').innerText
+    let output = document.getElementById('output').textContent
 
     output = input
     alert(input)
