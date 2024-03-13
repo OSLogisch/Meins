415b16a5de8ce04d770d356d46c5327d87c47184
OSLogisch <96527187+OSLogisch@users.noreply.github.com>
2024-03-13 12:57:40 +0100

    1

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const input = document.getElementById('input')
 const output = document.getElementById('output')
 
 form.addEventListener('submit' , (e) => {
+    e.preventDefault()
     let eingabe = input.value
 })
 
