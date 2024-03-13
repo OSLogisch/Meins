1878e7ed6c578cc86769e6c92d93a0098c6455ff
OSLogisch <96527187+OSLogisch@users.noreply.github.com>
2024-03-13 10:47:06 +0100

    0

--- script.js
+++
@@ -1,9 +0,0 @@
-let datum = new Date()
-const url = 'https://lgh.today/plan/2024-03-01:2'; 
-
-// Fetch Request
-fetch(url)
-  .then(response => response.text())
-  .then(html => {
-    document.body.innerHTML = html;
-  });
